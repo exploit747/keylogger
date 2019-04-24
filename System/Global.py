94fe7137898b36dd31ff331ba3c3129539d7e3cf
B374K <44334553+exploit747@users.noreply.github.com>
2019-04-24 12:35:55 +0700

    Update Global.py

--- System/Global.py
+++ System/Global.py
@@ -2,6 +2,6 @@ import os
 
 MAINPATH 	= os.path.abspath(os.path.join(os.path.dirname(__file__)))
 VERSION 	= '1.0.0'
-GITHUB		= 'https://github.com/enddo'
-CODER		= 'Farzin Enddo'
+GITHUB		= 'https://github.com/exploit/keylogger'
+CODER		= 'Exploit747'
 AGENTS 		= list()
